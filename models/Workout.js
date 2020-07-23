85dcf97aa0886f06aebaa122367aced805bb4583
Ryan Barwise <ryanbarwise@gmail.com>
2020-07-23 18:15:25 -0400

    added virtual to model to get total duration

--- models/Workout.js
+++ models/Workout.js
@@ -2,57 +2,54 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
-  },
-
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: true,
-      },
-      name: {
-        type: String,
-        trim: true,
-        required: true,
-      },
-      duration: {
-        type: Number,
-        required: true,
-      },
-      weight: {
-        type: Number
-      },
-      reps: {
-        type: Number
-      },
-      sets: {
-        type: Number
-      },
-      distance: {
-        type: Number
-      },
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-  ],
+
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: true,
+        },
+        duration: {
+          type: Number,
+          required: true,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+  }
+);
+
+WorkoutSchema.virtual('totalDuration').get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
-//add a property to schema use property called virtual
-// {
-//   day: new Date().setDate(new Date().getDate()-10),
-//   exercises: [
-//     {
-//       type: "resistance",
-//       name: "Bicep Curl",
-//       duration: 20,
-//       weight: 100,
-//       reps: 10,
-//       sets: 4
-//     }
-//   ]
-// },
 
 const Workout = mongoose.model('Workout', WorkoutSchema);
 
